6cf12e39c2b24bc28c02f7dfbceebc98f5297a35
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-08 01:30:25 +0530

    Welcome Message

---
+++ src/oopsproblems/Account.java
@@ -0,0 +1,11 @@
+package oopsproblems;
+
+public class Account {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Account Withdrawl");
+		
+	}
+
+}
